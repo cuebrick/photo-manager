c8d9a0c5bd3e045e7df09b6816b6eca96ff30bb2
Jong-sun Won <cuebrick@gmail.com>
2018-08-19 08:41:45 +0900

    Add Main.jsx, Settings.jsx files.

---
+++ src/renderer/Settings.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-dom';
+
+export default class Settings extends React.Component{
+	render(){
+		return(
+			<div>Settings page</div>
+		)
+	}
+}
