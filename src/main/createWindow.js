ec1b094e10e7a2e15f9e66d33771d3a6bd632976
Jong-sun Won <cuebrick@gmail.com>
2018-08-19 08:57:14 +0900

    Edit window size, add dev tools on createWindow.js.

--- src/main/createWindow.js
+++ src/main/createWindow.js
@@ -3,11 +3,12 @@ import { BrowserWindow } from 'electron';
 let win;
 
 function createWindow() {
-	win = new BrowserWindow();
+	win = new BrowserWindow({ width: 1600, height: 800 });
 	win.loadURL(__dirname + '/../../index.html');
 	win.on('close', () => {
 		win = null;
 	});
+	win.webContents.openDevTools();
 }
 
 export default createWindow;
