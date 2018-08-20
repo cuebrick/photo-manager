63f674bcb89636e4bbfa33872588ad3f446c59f7
Jong-sun Won <cuebrick@gmail.com>
2018-08-21 07:24:01 +0900

    Apply react routing.

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-render(<div>Photo Manager</div>, document.getElementById('app'));
+import Main from './Main';
+import Settings from './Settings';
+
+const routing = (
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" component={Main} />
+			<Route path="settings" component={Settings} />
+		</Switch>
+	</BrowserRouter>
+);
+
+render(routing, document.getElementById('app'));
