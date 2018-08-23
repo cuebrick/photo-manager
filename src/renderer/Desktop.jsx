b3b521801e347e1ca558738e64e772fe28272995
Jong-sun Won <cuebrick@gmail.com>
2018-08-24 07:34:03 +0900

    Add Desktop.jsx file.

---
+++ src/renderer/Desktop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Desktop extends React.Component{
+	render(){
+		return(
+			<div className="desktop">
+
+			</div>
+		)
+	}
+}
--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Main from './Main';
 import Settings from './Settings';
+import Desktop from "./Desktop";
 
 const routing = (
 	<BrowserRouter>
 		<Switch>
-			<Route path="/" component={Main} />
 			<Route path="settings" component={Settings} />
+			<Route path="desktop" component={Desktop} />
+			<Route component={Main} />
 		</Switch>
 	</BrowserRouter>
 );
