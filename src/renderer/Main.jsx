4529ece0d0811ca2d2f0a87c8ef313c2fd122d93
Jong-sun Won <cuebrick@gmail.com>
2018-08-25 08:09:01 +0900

    Edit Main.jsx file.

--- src/renderer/Main.jsx
+++ src/renderer/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Link} from 'react-dom';
+import { Link } from 'react-router-dom';
 
 export default class Main extends React.Component{
 	render(){
 		return(
-			<div>Main page</div>
+			<div>
+				<h1>Photo Manager</h1>
+				<div className="main-contents">
+					<p>Photo Manager 는 여러 곳에 나뉘어 있는 여러분의 사진을 한 곳에서 모아 보고 관리할 수 있도록 만들어진 프로그램 입니다.</p>
+				</div>
+			</div>
 		)
 	}
 }
