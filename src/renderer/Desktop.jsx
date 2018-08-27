4a588c62070dfa07527be1ee878688dbb63a814c
Jong-sun Won <cuebrick@gmail.com>
2018-08-28 07:29:28 +0900

    Add Navigation.jsx file.

--- src/renderer/Desktop.jsx
+++ src/renderer/Desktop.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 export default class Desktop extends React.Component{
 	render(){
 		return(
 			<div className="desktop">
-
+				<Navigation></Navigation>
 			</div>
 		)
 	}
