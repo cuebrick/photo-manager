9eaf0e169c0f2dc65d1410c826628b617abdbab4
Jong-sun Won <cuebrick@gmail.com>
2018-08-29 06:39:02 +0900

    Edit loadURL path on createWindow().

--- src/main/createWindow.js
+++ src/main/createWindow.js
@@ -4,7 +4,7 @@ let win;
 
 function createWindow() {
 	win = new BrowserWindow({ width: 1600, height: 800 });
-	win.loadURL(__dirname + '/../../index.html');
+	win.loadURL('file://' + __dirname + '/../../index.html');
 	win.on('close', () => {
 		win = null;
 	});
