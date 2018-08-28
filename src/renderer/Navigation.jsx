9d1a06985a9c011a4d81f3b6c3643ba12120036e
Jong-sun Won <cuebrick@gmail.com>
2018-08-29 07:39:14 +0900

    Markup on Navigation.jsx.

--- src/renderer/Navigation.jsx
+++ src/renderer/Navigation.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export default class Navigation extends React.Component{
 	render(){
 		return(
 			<nav>
-				navigation
+				<ul>
+					<li><Link to="/desktop">Desktop</Link></li>
+				</ul>
+				<ul>
+					<li><Link to="/settings">Settings</Link></li>
+				</ul>
 			</nav>
 		)
 	}
