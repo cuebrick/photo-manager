58d42786cebe71ef206674a51b1f4532d1b98032
Jong-sun Won <cuebrick@gmail.com>
2018-08-31 07:22:01 +0900

    Add interaction in Navigation.jsx

--- src/renderer/Navigation.jsx
+++ src/renderer/Navigation.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 export default class Navigation extends React.Component{
+	constructor(){
+		super();
+		this.menuIconClicked = this.menuIconClicked.bind(this);
+
+		this.state = {
+			isShow : false
+		}
+	}
+	menuIconClicked(){
+		this.setState({isShow:!this.state.isShow});
+	}
 	render(){
+		let cssShow = (this.state.isShow) ? ' active' : ''
 		return(
-			<nav>
-				<ul>
+			<nav className={cssShow}>
+				<div className={"menu-icon" + cssShow} onClick={this.menuIconClicked}>
+					<div className="menu-bar"></div>
+				</div>
+				<ul className="menu">
 					<li><Link to="/desktop">Desktop</Link></li>
-				</ul>
-				<ul>
 					<li><Link to="/settings">Settings</Link></li>
 				</ul>
 			</nav>
