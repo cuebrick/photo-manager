e2365ae2607caa1415b45ccd3bdd6cc1983d989d
Jong-sun Won <cuebrick@gmail.com>
2018-09-01 07:04:17 +0900

    CSS styling in .wrapper, .container

--- src/renderer/Main.jsx
+++ src/renderer/Main.jsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 export default class Main extends React.Component{
 	render(){
 		return(
-			<div>
-				<h1>Photo Manager</h1>
-				<div className="main-contents">
+			<div className="container">
+				<div className="main-splash">
+					<h1>Photo Manager</h1>
 					<p>Photo Manager 는 여러 곳에 나뉘어 있는 여러분의 사진을 한 곳에서 모아 보고 관리할 수 있도록 만들어진 프로그램 입니다.</p>
+					<Link to="/desktop">ddd</Link>
 				</div>
 			</div>
 		)
--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -6,16 +6,20 @@ import Main from './Main';
 import Settings from './Settings';
 import Desktop from "./Desktop";
 import NotFound from "./NotFound";
+import Navigation from "./Navigation";
 
 
 const routing = (
 	<MemoryRouter>
-		<Switch>
-			<Route path="/settings" component={Settings} />
-			<Route path="/desktop" component={Desktop} />
-			<Route component={Main} />
-			<Route component={NotFound} />
-		</Switch>
+		<div className="wrapper">
+			<Navigation />
+			<Switch>
+				<Route path="/settings" component={Settings} />
+				<Route path="/desktop" component={Desktop} />
+				<Route component={Main} />
+				<Route component={NotFound} />
+			</Switch>
+		</div>
 	</MemoryRouter>
 );
 
