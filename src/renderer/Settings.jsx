d6f5f2fa56e690394757556ded8b3aa3b791c497
Jong-sun Won <cuebrick@gmail.com>
2018-09-09 08:51:37 +0900

    Add location ui in Settings.jsx file.

--- src/renderer/Settings.jsx
+++ src/renderer/Settings.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-dom';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class Settings extends React.Component{
 	render(){
@@ -9,8 +12,22 @@ export default class Settings extends React.Component{
 				<h2>Storage</h2>
 				<h3>Add storage</h3>
 				<div>
-					<input type="text"/>
-					<button>Add</button>
+					<TextField
+						id="name"
+						label="Path in this device."
+						margin="normal"
+					/>
+					<Button variant="contained" color="primary">
+						Add folder
+					</Button>
+				</div>
+				<h4>Saved location</h4>
+				<div>
+					<ul>
+						<li>C:\Users\user\Desktop\Works\PhotoManager</li>
+						<li>D:\Photo</li>
+						<li>C:\Users\user\Pictures</li>
+					</ul>
 				</div>
 			</div>
 		)
