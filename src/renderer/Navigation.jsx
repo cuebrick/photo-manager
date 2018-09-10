611da15e694b25800dec876c26888250f01cbe1e
Jong-sun Won <cuebrick@gmail.com>
2018-09-11 08:05:54 +0900

    Apply material-ui on Navigation.

--- src/renderer/Navigation.jsx
+++ src/renderer/Navigation.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Switch from '@material-ui/core/Switch';
+import WifiIcon from '@material-ui/icons/Wifi';
+import BluetoothIcon from '@material-ui/icons/Bluetooth';
+
+
 export default class Navigation extends React.Component{
-	constructor(){
-		super();
+	constructor(props){
+		super(props);
 		this.menuIconClicked = this.menuIconClicked.bind(this);
 
 		this.state = {
 			isShow : false
 		}
 	}
+
+
 	menuIconClicked(){
 		this.setState({isShow:!this.state.isShow});
 	}
@@ -20,12 +35,35 @@ export default class Navigation extends React.Component{
 				<div className={"menu-icon" + cssShow} onClick={this.menuIconClicked}>
 					<div className="menu-bar"></div>
 				</div>
-				<ul className={"menu" + cssShow}>
-					<li><Link to="/desktop">Desktop</Link></li>
-					<li><Link to="/settings">Settings</Link></li>
 
-					<li><Link to="/add-storage">Add Storage</Link></li>
-				</ul>
+				<div className={"menu" + cssShow}>
+					<ul>
+						<li><Link to="/desktop">Desktop</Link></li>
+						<li><Link to="/settings">Settings</Link></li>
+
+						<li><Link to="/add-storage">Add Storage</Link></li>
+					</ul>
+					<List>
+						<ListItem>
+							<ListItemIcon>
+								<WifiIcon />
+							</ListItemIcon>
+							<ListItemText primary="Wi-Fi" />
+							<ListItemSecondaryAction>
+								<Switch />
+							</ListItemSecondaryAction>
+						</ListItem>
+						<ListItem>
+							<ListItemIcon>
+								<BluetoothIcon />
+							</ListItemIcon>
+							<ListItemText primary="Bluetooth" />
+							<ListItemSecondaryAction>
+								<Switch />
+							</ListItemSecondaryAction>
+						</ListItem>
+					</List>
+				</div>
 			</nav>
 		)
 	}
