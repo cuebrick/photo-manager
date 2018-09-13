cc841fa31886729bb87cfe92dd41f7f8d58deec3
Jong-sun Won <cuebrick@gmail.com>
2018-09-14 08:08:24 +0900

    Testing load files.

--- src/renderer/AddStorage.jsx
+++ src/renderer/AddStorage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import fs from "fs";
 
 export default class AddStorage extends React.Component{
 	constructor(props){
 		super(props);
+		fs.readdir('./.tmp/photo/samples/', (err, dir) => {
+			for(let path of dir){
+				console.log(path);
+			}
+		});
 	}
 	render(){
 		return(
