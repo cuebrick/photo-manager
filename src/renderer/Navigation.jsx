e794355fcc9132f98854ce103bf2e933960cf1db
Jong-sun Won <cuebrick@gmail.com>
2018-09-19 04:51:18 +0900

    CSS styling in Navigation.jsx file.

--- src/renderer/Navigation.jsx
+++ src/renderer/Navigation.jsx
@@ -10,30 +10,23 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Switch from '@material-ui/core/Switch';
 import SettingsIcon from '@material-ui/icons/Settings';
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
 import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import Tooltip from '@material-ui/core/Tooltip';
 
-/*const styles = createStyles({
-	root: {
-		colorPrimary: '#fff'
-	},
-	icon: {
-		marginRight: 0
-	},
-	item: {
-		color: '#999',
-		padding: 10,
-		cursor: 'default'
-	},
-	itemText: {
-		color: '#9db2cc',
-		padding: '0 10px'
-	}
-});*/
-
 const theme = createMuiTheme({
 	overrides: {
+		MuiListItem: {
+			gutters: {
+				paddingLeft: 0
+			}
+		},
+		MuiDivider: {
+			root: {
+				backgroundColor: 'rgba(255,255,255,0.1)'
+			}
+		},
 		MuiListItemText: {
 			root: {
 				padding: 0,
@@ -70,48 +63,49 @@ export default class Navigation extends React.Component{
 	render(){
 		let cssShow = (this.state.isShow) ? ' active' : ''
 		return(
-			
 			<MuiThemeProvider theme={theme}>
-			<nav className={cssShow}>
-				<div className={"menu-icon" + cssShow} onClick={this.menuIconClicked}>
-					<div className="menu-bar" />
-				</div>
-				<div className={"menu" + cssShow}>
-					<List>
-						<ListItem button component={Link} to="/desktop">
-							<ListItemIcon>
-								<CameraAltIcon />
-							</ListItemIcon>
-							<ListItemText primary="Desktop" />
-						</ListItem>
-						<Divider />
-						<ListItem>
-							<ListItemIcon>
-								<CameraAltIcon />
-							</ListItemIcon>
-							<Tooltip title="D://Photo" placement="right">
-								<ListItemText primary="Photo" />
-							</Tooltip>
-							<ListItemSecondaryAction>
-								<Switch />
-							</ListItemSecondaryAction>
-						</ListItem>
-						<Divider />
-						<ListItem button component={Link} to="/add-storage">
-							<ListItemIcon>
-								<AddAPhotoIcon />
-							</ListItemIcon>
-							<ListItemText primary="Add Storage" />
-						</ListItem>
-						<ListItem button component={Link} to="/settings">
-							<ListItemIcon>
-								<SettingsIcon />
-							</ListItemIcon>
-							<ListItemText primary="Settings" />
-						</ListItem>
-					</List>
-				</div>
-			</nav>
+				<nav className={cssShow}>
+					<div className={"menu-icon" + cssShow} onClick={this.menuIconClicked}>
+						<div className="menu-bar" />
+					</div>
+					<div className={"menu" + cssShow}>
+						<List>
+							<Divider />
+							<ListItem button component={Link} to="/desktop">
+								<ListItemIcon>
+									<DesktopWindowsIcon />
+								</ListItemIcon>
+								<ListItemText primary="Desktop" />
+							</ListItem>
+							<Divider />
+							<ListItem>
+								<ListItemIcon>
+									<CameraAltIcon />
+								</ListItemIcon>
+								<Tooltip title="D://Photo" placement="right">
+									<ListItemText primary="Photo" />
+								</Tooltip>
+								<ListItemSecondaryAction>
+									<Switch />
+								</ListItemSecondaryAction>
+							</ListItem>
+							<Divider />
+							<ListItem button component={Link} to="/add-storage">
+								<ListItemIcon>
+									<AddAPhotoIcon />
+								</ListItemIcon>
+								<ListItemText primary="Add Storage" />
+							</ListItem>
+							<ListItem button component={Link} to="/settings">
+								<ListItemIcon>
+									<SettingsIcon />
+								</ListItemIcon>
+								<ListItemText primary="Settings" />
+							</ListItem>
+							<Divider />
+						</List>
+					</div>
+				</nav>
 			</MuiThemeProvider>
 		)
 	}
