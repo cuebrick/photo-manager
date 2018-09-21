f73b43c224fae88189d2a23cdf3c61ad29cb769a
Jong-sun Won <cuebrick@gmail.com>
2018-09-21 16:02:47 +0900

    Open search folder dialog.

--- src/renderer/AddStorage.jsx
+++ src/renderer/AddStorage.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import {dialog} from 'electron-remote';
 import fs from "fs";
+
 
 export default class AddStorage extends React.Component{
 	constructor(props){
 		super(props);
-		fs.readdir('./.tmp/photo/samples/', (err, dir) => {
+		/*fs.readdir('./.tmp/photo/samples/', (err, dir) => {
 			for(let path of dir){
 				console.log(path);
 			}
-		});
+		});*/
 	}
+	
+	selectDirectory(){
+		// let remote = require('remote');
+		// let dialog = remote.require('electron').dialog;
+		dialog.showOpenDialog({
+			properties: ['openDirectory']
+		})
+	}
+	
 	render(){
 		return(
 			<div className="container">
-
+				<button onClick={this.selectDirectory}>Add Folder</button>
 			</div>
 		)
 	}
