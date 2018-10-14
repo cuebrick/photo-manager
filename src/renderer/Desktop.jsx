f63addab5843aa1e1105f1cd45ada83cc6829e6e
Jong-sun Won <cuebrick@gmail.com>
2018-10-15 07:23:12 +0900

    CSS color styling on Desktop.jsx file.

--- src/renderer/Desktop.jsx
+++ src/renderer/Desktop.jsx
@@ -5,11 +5,14 @@ export default class Desktop extends React.Component{
 		return(
 			<div className="container">
 				<div className="photo-group">
-					<img src="../../sample-photo/pixabay/dawn-3358468_1920.jpg" alt="dawn"/>
-					<img src="../../sample-photo/pixabay/fall-foliage-3705550_1920.jpg" alt="fall foliage"/>
-					<img src="../../sample-photo/pixabay/road-1072823_1920.jpg" alt="road"/>
-					<img src="../../sample-photo/pixabay/terminalia-catappa-2137221_1920.jpg" alt="terminalia catappa"/>
-					<img src="../../sample-photo/pixabay/wheat-3506758_1920.jpg" alt="wheat"/>
+					<div className="group-ui">
+						<h5>This is group name</h5>
+					</div>
+					<img src="sample-photo/pixabay/dawn-3358468_1920.jpg" alt="dawn"/>
+					<img src="sample-photo/pixabay/fall-foliage-3705550_1920.jpg" alt="fall foliage"/>
+					<img src="sample-photo/pixabay/road-1072823_1920.jpg" alt="road"/>
+					<img src="sample-photo/pixabay/terminalia-catappa-2137221_1920.jpg" alt="terminalia catappa"/>
+					<img src="sample-photo/pixabay/wheat-3506758_1920.jpg" alt="wheat"/>
 				</div>
 			</div>
 		)
